174d3dd5b1bc032e3ce80a958d60903a3331b40b
Petr Havlicek <havlicekp@gmail.com>
2019-04-24 18:22:25 +0200

    Introduced TDirections as a set of TDirection. Combined directions like dUpLeft removed.

--- src/FormDesigner.Package/FormDesigner.Interfaces.pas
+++ src/FormDesigner.Package/FormDesigner.Interfaces.pas
@@ -2,16 +2,19 @@ unit FormDesigner.Interfaces;
 
 interface
 
-uses Classes,Controls, Graphics, Windows, Messages, Forms, SysUtils, StdCtrls, System.Generics.Collections;
+uses Classes, Controls, Graphics, Windows, Messages, Forms, SysUtils, StdCtrls,
+  System.Generics.Collections;
 
 type
 
-  TFormDesignerState = (ssReady, ssMoving, ssSizing, ssSizingNewControl);
-  TDirections = (dDown,dUp,dLeft,dRight,dUpLeft,dUpRight,dDownLeft,dDownRight,dPosChange);
+  TFormDesignerState = (ssReady, ssMoving, ssSizing);
+  TDirection = (dBottom, dTop, dLeft, dRight, dUpLeft, dUpRight, dDownLeft,
+    dDownRight, dPosChange);
+  TDirections = set of TDirection;
 
   IFormDesigner = interface
-    function GetRect(): TRect;
-    function GetChildRect(): TRect;
+    function GetRect : TRect;
+    function GetChildRect : TRect;
     procedure UpdateRect(Rect: TRect; Direction: TDirections);
   end;
 
